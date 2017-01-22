d1793cda946579871c04135c701b5b2b61193098
philly <sonagi.philly@gmail.com>
2017-01-22 21:15:52 +0900

    Implement Bot

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import tweepy
 import datetime
+from random import choice
+from time import sleep
+from github import Github
+usr_name = '@adnim'
 
 f = open('key.config')
-cons_key, cons_sec, tok_key, tok_sec = f.read().split()
+cons_key, cons_sec, tok_key, tok_sec, github_id, github_pw = f.read().split()
 auth = tweepy.OAuthHandler(cons_key, cons_sec)
 auth.set_access_token(tok_key, tok_sec)
 api = tweepy.API(auth)
 
+user = Github(github_id, github_pw)
+
+msg_list = open('messages.txt', encoding='utf-8-sig').read().split('\n')
+
 def tweet(msg):
     api.update_status(msg)
 
-def get_today_commits(user):
-    today = datetime.datetime.today()
-    today_date = datetime.datetime(today.year, today.month, today.day)
-    today_date_ko = today_date - datetime.timedelta(hours=9)
+def today():
+    t = datetime.datetime.today()
+    t_d = datetime.datetime(t.year, t.month, t.day)
+    return t_d - datetime.timedelta(hours=9)
 
+def get_today_commits():
     for event in user.get_events():
-        if event.created_at > today_date_ko:
+        if event.created_at > today():
             if event.type in ['PushEvent', 'PullRequestEvent', 'IssueEvent']:
                 yield event
         else:
             break
+
+def handle():
+    #if len(list(get_today_commits())) == 0:
+        tweet(usr_name + ' ' + choice(msg_list))
+        print('Tweet sent!')
+
+while True:
+    if today().hour > 14:
+        handle()
+    sleep(30)
